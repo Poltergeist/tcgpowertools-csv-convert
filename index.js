8595ae683c6da076ce90c23bb22d570a256c601b
Poltergeist <philipp@sp3c1.com>
2024-11-30 21:04:13 +0100

    update

--- index.js
+++ index.js
@@ -1,10 +1,70 @@
+#!/usr/bin/env node
 const fs = require("fs");
 const papa = require("papaparse");
 const ordersPath = process.argv[2];
+const outputPath = process.argv[3] || "./";
 const countryCode = {
   D: "DEU",
   AT: "AT",
 };
+
+if (outputPath[outputPath.length - 1] !== "/") {
+  outputPath += "/";
+}
+if (!fs.existsSync(outputPath)) {
+  console.log("Invalid output path")
+  process.exit(1)
+}
+
+function replaceNonLatinChars(str) {
+  const charMap = {
+    // Polish
+    'Ł': 'L', 'ł': 'l',
+    'Ą': 'A', 'ą': 'a',
+    'Ć': 'C', 'ć': 'c',
+    'Ę': 'E', 'ę': 'e',
+    'Ń': 'N', 'ń': 'n',
+    'Ś': 'S', 'ś': 's',
+    'Ź': 'Z', 'ź': 'z',
+    'Ż': 'Z', 'ż': 'z',
+
+    // German (excluding Latin-1)
+    'ß': 'ss',
+
+    // French (excluding Latin-1)
+    'Œ': 'OE', 'œ': 'oe',
+    'Ÿ': 'Y', 'ÿ': 'y',
+
+    // Spanish (excluding Latin-1)
+    'Ñ': 'N', 'ñ': 'n',
+
+    // Other European characters
+    'Č': 'C', 'č': 'c',
+    'Š': 'S', 'š': 's',
+    'Ž': 'Z', 'ž': 'z',
+    'Đ': 'Dj', 'đ': 'dj',
+    'ě': 'e', 'Ě': 'E',
+    'Ď': 'D', 'ď': 'd',
+    'Ň': 'N', 'ň': 'n',
+    'Ť': 'T', 'ť': 't',
+    'Ő': 'O', 'ő': 'o',
+    'Ű': 'U', 'ű': 'u',
+    'Ğ': 'G', 'ğ': 'g',
+    'Ş': 'S', 'ş': 's',
+    'İ': 'I', 'ı': 'i',
+    'ė': 'e', 'Ė': 'E',
+
+    // Scandinavian (excluding Latin-1)
+    'Ø': 'O', 'ø': 'o',
+    'Æ': 'AE', 'æ': 'ae',
+
+    // some more signs
+    'º': 'o',
+  };
+
+  return str.split('').map(char => charMap[char] || char).join('');
+}
+
 
 papa.parse(fs.readFileSync(ordersPath, { encoding: "utf8" }).toString(), {
   delimiter: ",",
@@ -29,12 +89,12 @@ papa.parse(fs.readFileSync(ordersPath, { encoding: "utf8" }).toString(), {
       acc[cur.shippingMethod] = [
         ...acc[cur.shippingMethod],
         {
-          NAME: cur.shippingAddressName,
-          ZUSATZ: cur.shippingAddressExtra || "",
-          STRASSE: cur.shippingAddressStreet,
+          NAME: replaceNonLatinChars(cur.shippingAddressName),
+          ZUSATZ: replaceNonLatinChars(cur.shippingAddressExtra) || "",
+          STRASSE: replaceNonLatinChars(cur.shippingAddressStreet),
           NUMMER: "",
-          PLZ: cur.shippingAddressZip,
-          STADT: cur.shippingAddressCity,
+          PLZ: replaceNonLatinChars(cur.shippingAddressZip),
+          STADT: replaceNonLatinChars(cur.shippingAddressCity),
           LAND:
             countryCode[cur.shippingAddressCountry] ||
             cur.shippingAddressCountry,
@@ -45,11 +105,11 @@ papa.parse(fs.readFileSync(ordersPath, { encoding: "utf8" }).toString(), {
       return acc;
     }, {});
     const date = Date.now();
-    fs.mkdirSync("./" + date);
+    fs.mkdirSync(outputPath + date);
     Object.keys(data).forEach((key) => {
       console.log(key, data[key].length - 1);
       fs.writeFileSync(
-        `./${date}/${key}.csv`,
+        `${outputPath}${date}/${key}.csv`,
         papa.unparse(data[key], { delimiter: ";" }) + "\n",
         "binary"
       );
